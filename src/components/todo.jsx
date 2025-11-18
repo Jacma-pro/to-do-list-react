7b03ff89bb570736ae4f36754e95fa41e50b515f
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-11-18 14:43:30 +0100

    add handleAddTask in the return

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -3,10 +3,10 @@
 import { useState } from 'react';
 
 const Todo = () => {
-  const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState('');
+    const [tasks, setTasks] = useState(["test chair", "eat Pom'Potes", "Watch Nouvelle Ecole"]);
+    const [input, setInput] = useState('');
 
-const handleAddTask = () => {
+    const handleAddTask = () => {
         if (input) {
             setTasks([input, ...tasks]);
             setInput('');
@@ -17,24 +17,31 @@ const handleAddTask = () => {
             handleAddTask();
         }
     }
-  return (
-    <>
-      <h1>Todo List</h1>
-      <div className="todo-container">
-        <div className="input-section">
-          <input className='task-input'
-            type="text"
-          >
-          </input>
-          <button className='add-task-button'>Add Task</button>
-        </div>
-        <div className="task-section">
-            <ul className='task-list'>
-                
-            </ul>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>Todo List</h1>
+            <div className="todo-container">
+                <div className="input-section">
+                    <input className='task-input'
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={handleEnterKey}
+                    />
+                    <button className='add-task-button'
+                        onClick={handleAddTask}
+                        
+                    >Add Task</button>
+                </div>
+                <div className="task-section">
+                    <ul className='task-list'>
+                        {tasks.map((task, index) => (
+                            <li key={index} className='task-item'>{task}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 }
 export default Todo;
